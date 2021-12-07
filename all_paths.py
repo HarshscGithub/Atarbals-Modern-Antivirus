79fc831155ae780b7e51a6ba55e725cc969c1fea
Harsh Chaudhari <90396120+HarshscGithub@users.noreply.github.com>
2021-12-07 11:07:20 +0530

    Add files via upload

---
+++ all_paths.py
@@ -0,0 +1,16 @@
+import os
+  
+Picture_path = os.path.abspath("No_Virus_Found.png")
+
+icon_path = os.path.abspath("Antivirus_Logo.png")
+
+#First time run paths  
+
+username_file_path = os.path.abspath("User_credn/usernames.txt")
+
+password_file_path = os.path.abspath("User_credn/password.txt")
+
+email_file_path = os.path.abspath("User_credn/email.txt")
+
+purchase_file_path = os.path.abspath("User_credn/purchase.txt")
+
